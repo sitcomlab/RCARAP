1208297520fafa9f2f9be57249517bea43d913f9
Philipp Glahe <p_glah01@uni-muenster.de>
2018-05-01 14:47:36 +0200

    added video example

--- public/js/connect-peer.js
+++ public/js/connect-peer.js
@@ -8,6 +8,9 @@ var peer = new Peer({
     debug:3
 
 });
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
 /**
 document.addEventListener("DOMContentLoaded", function(event) {
     peer.on('open', function () {
@@ -89,4 +92,67 @@ $(document).ready(function() {
         $('#text').val('');
     });
 });
+
+peer.on('open', function(){
+    $('#my-id').text(peer.id);
+});
+
+peer.on('call', function(call){
+    // Answer the call automatically (instead of prompting user) for demo purposes
+    call.answer(window.localStream);
+    step3(call);
+});
+peer.on('error', function(err){
+    alert(err.message);
+    // Return to step 2 if error occurs
+    step2();
+});
+// Click handlers setup
+$(function(){
+    $('#make-call').click(function(){
+        // Initiate a call!
+        var call = peer.call($('#callto-id').val(), window.localStream);
+        step3(call);
+    });
+    $('#end-call').click(function(){
+        window.existingCall.close();
+        step2();
+    });
+    // Retry if getUserMedia fails
+    $('#step1-retry').click(function(){
+        $('#step1-error').hide();
+        step1();
+    });
+    // Get things started
+    step1();
+});
+function step1 () {
+    // Get audio/video stream
+    navigator.getUserMedia({audio: true, video: true}, function(stream){
+        // Set your video displays
+        $('#my-video').prop('src', URL.createObjectURL(stream));
+        window.localStream = stream;
+        step2();
+    }, function(){ $('#step1-error').show(); });
+}
+function step2 () {
+    $('#step1, #step3').hide();
+    $('#step2').show();
+}
+function step3 (call) {
+    // Hang up on an existing call if present
+    if (window.existingCall) {
+        window.existingCall.close();
+    }
+    // Wait for stream on the call, then set peer video display
+    call.on('stream', function(stream){
+        $('#their-video').prop('src', URL.createObjectURL(stream));
+    });
+    // UI stuff
+    window.existingCall = call;
+    $('#their-id').text(call.peer);
+    call.on('close', step2);
+    $('#step1, #step2').hide();
+    $('#step3').show();
+}
 
